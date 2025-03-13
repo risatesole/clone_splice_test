18a171b3063054e206b4be798842cf34ec58c89e
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2025-03-13 19:04:46 -0400

    refactor: removing unused function to change text logo with button

--- vanilla/scripts.js
+++ vanilla/scripts.js
@@ -5,11 +5,3 @@ const logoText = document.getElementById("logo_text");
 if (logoText) {
     logoText.textContent = websiteTitle;
 }
-
-// Add event listener to change logo text on button click
-const changeLogoButton = document.getElementById("changeLogoButton");
-if (changeLogoButton) {
-    changeLogoButton.addEventListener("click", () => {
-        logoText.textContent = "New Logo Text"; // Change the text
-    });
-}
